a54bd348431b3a4fa7f33a196b31c6194d09a4d6
Afaan Bilal <afaanbilal@gmail.com>
2023-06-09 18:17:38 +0530

    ✨ Masjid Duas

--- src/screens/Checklists.tsx
+++ src/screens/Checklists.tsx
@@ -17,8 +17,10 @@ const checklists: Checklist[] = [
     { id: '1', title: 'Before sleeping', duas: [{ dua: duas[11], rep: 1 }, { dua: duas[3], rep: 33 }, { dua: duas[4], rep: 33 }, { dua: duas[5], rep: 34 },], },
     { id: '2', title: 'Waking up', duas: [{ dua: duas[4], rep: 3 }, { dua: duas[12], rep: 1 },], },
     { id: '3', title: 'Leaving home', duas: [{ dua: duas[16], rep: 1 },], },
-    { id: '4', title: 'Before entering washroom', duas: [{ dua: duas[14], rep: 1 },], },
-    { id: '5', title: 'After exiting washroom', duas: [{ dua: duas[15], rep: 1 },], },
+    { id: '4', title: 'Entering masjid', duas: [{ dua: duas[17], rep: 1 },], },
+    { id: '5', title: 'Leaving masjid', duas: [{ dua: duas[18], rep: 1 },], },
+    { id: '6', title: 'Before entering washroom', duas: [{ dua: duas[14], rep: 1 },], },
+    { id: '7', title: 'After exiting washroom', duas: [{ dua: duas[15], rep: 1 },], },
 ];
 
 const Checklists: React.FC = () => {
